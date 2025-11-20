d7621224bb3de7f87e36fa35a4d24292744feeeb
Dexied <33525453+Dexied@users.noreply.github.com>
2025-11-21 01:28:49 +0500

    570-000

--- PluginBuild.bat
+++ PluginBuild.bat
@@ -1,1 +1,5 @@
-B:\UnrealEngine\UE_5.6\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\AISpawnerDemo\Plugins\AISpawner\AISpawner.uplugin" -Package="B:\Projects\AISpawnerDemo\Builds\AISpawner" -Rocket -2019
+@echo off
+
+B:\UnrealEngineSource\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="B:\Projects\AISpawnerDemo\Plugins\AISpawner\AISpawner.uplugin" -Package="B:\Projects\AISpawnerDemo\Builds\AISpawner" -Rocket -2019 > "B:\Projects\AISpawnerDemo\BuildLog.txt" 2>&1
+echo Build completed. Check BuildLog.txt for details.
+pause
